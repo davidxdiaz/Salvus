74d4813bee1a92722ac79b1bcd75b2da6855f962
Andy Escobar <andyescobar007@gmail.com>
2019-03-06 13:43:30 -0600

    View Login :+1:

---
+++ Salvus/Salvus/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Salvus.Models
+{
+    public class User
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+
+        public User(string username,string password)
+        {
+            this.UserName = username;
+            this.Password = password;
+        }
+
+    }
+}
